d657740537dc9ad32f1d0981cc497d10480a7b87
Devin Carraway <git@devin.com>
2023-10-07 22:58:50 -0700

    Initial import

---
+++ README.md
@@ -0,0 +1,4 @@
+A simple Prometheus exporter for 1-wire temperature sensors.
+
+Works with DS18S20 devices connected directly, or attached to a DS2482 bus
+master.
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"math"
+	"net/http"
+	"strconv"
+
+	"github.com/aqua/raspberrypi/onewire"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var listen = flag.String("listen", ":9456", "(Host and) port to listen on for Prometheus export")
+var onlyDevice = flag.String("only-device", "", "Only report the given device ID (for testing)")
+
+func export(devices []*onewire.DS18S20) {
+	for i := range devices {
+		func(d *onewire.DS18S20) {
+			prometheus.MustRegister(prometheus.NewGaugeFunc(
+				prometheus.GaugeOpts{
+					Namespace: "onewire",
+					Name:      "temperature_degrees_celsius",
+					Help:      "Temperature sampled from a single sensor, in degrees celsius",
+					ConstLabels: prometheus.Labels{
+						"id":     strconv.FormatUint(d.Id, 10),
+						"device": d.HumanId(),
+						"model":  d.Model(),
+					},
+				},
+				func() float64 {
+					md, err := d.Read()
+					if err != nil {
+						log.Printf("Error reading %s: %v", d.HumanId(), err)
+						return math.NaN()
+					}
+					return float64(md) / 1000.
+				}))
+		}(devices[i])
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	ids, err := onewire.Scan()
+	if err != nil {
+		log.Fatalf("Unable to scan for 1wire devices: %v", err)
+	}
+	if *onlyDevice != "" {
+		ids = []string{*onlyDevice}
+	}
+
+	devices := make([]*onewire.DS18S20, len(ids))
+	for i, id := range ids {
+		func(i int, ID string) {
+			if devices[i], err = onewire.NewDS18S20(ID); err != nil {
+				log.Printf("Error opening %s as a DS18S20 device (%v); skipping it", id, err)
+			}
+		}(i, id)
+	}
+	export(devices)
+
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(*listen, nil))
+}
